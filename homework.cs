6470087a3ea487eed7eb861d616c6fd312effd35
Kotikov <11deadsluts@gmail.com>
2022-10-23 17:14:16 +0300

    2nd task is solved

--- homework.cs
+++ homework.cs
@@ -1,4 +1,6 @@
-﻿void palindrome(int x)
+﻿//Задача (1) 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+void palindrome(int x)
 {
     int digit1=x%10; // 12321 = 1
     int digit2=(x%100)/10; // 12321=21=2
@@ -17,3 +19,28 @@ System.Console.WriteLine("Input only a 5-digit number");
 int num = Convert.ToInt32(Console.ReadLine());
 
 palindrome(num);
+*/
+// Задача (2) 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double DistanceBetweenTwoDots (int xa, int xb, int ya, int yb, int za, int zb)
+{
+    double ab = Math.Sqrt(((xb-xa)*(xb-xa)) + ((yb-ya)*(yb-ya)) + ((zb-za)*(zb-za)));
+    return ab;
+}
+
+System.Console.WriteLine("Input coordinate xa: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input coordinate xb: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input coordinate ya: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input coordinate yb: ");
+int n4 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input coordinate za: ");
+int n5 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input coordinate zb: ");
+int n6 = Convert.ToInt32(Console.ReadLine());
+
+double result = DistanceBetweenTwoDots (n1,n2,n3,n4,n5,n6);
+
+System.Console.WriteLine($"The distance between two dots in 3D space is {result}");
